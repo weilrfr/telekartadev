85f202ef711db33bdc224b78d8c55272255d34c4
weilrfr <boyarkinid3@gmail.com>
2025-10-13 19:40:40 +0500

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Header from './components/layouts/Header.vue'
+import Footer from './components/layouts/Footer.vue'
 </script>
 
 <template>
   <Header />
   <RouterView/>
+  <Footer />
 </template>
 
 <style scoped>
---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+</script>
+<template>
+    <section>
+        <div class="cont">
+            <div class="footer">
+                <h1>Telekartatv</h1>
+                <div>
+                    <p>+7 (965) 354 37 37</p>
+                    <p>+7 (962) 821 37 37</p>
+                    <p>+7 (963) 521 37 37</p>
+                    <p>© 2025 Партнеры спутникового телевидения |  Уверенная зона покрытия России, Казахстана, Кыргызстана</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+    section {
+        display: flex;
+        justify-content: center;
+        background-color: rgba(212, 15, 125, 1);
+        color: white;
+        margin-top: 25vh;
+    }
+    .cont {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        max-width: 1300px;
+    }
+    .footer {
+        display: flex;
+        background-color: rgba(212, 15, 125, 1);
+        flex-wrap: wrap;
+        align-items: flex-end;
+        width: 100%;
+        text-align: center;
+    }
+    .footer h1 {
+        font-size: 2em;
+        width: 100%;
+        text-align: center;
+        margin-top: 50px;
+    }
+    .footer div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        flex-basis: 100%;
+        text-align: center;
+    }
+    .footer p {
+        margin: 2px 0;
+    }
+</style>
